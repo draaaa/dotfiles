3be0c9fd40867ed5c93cd5c75fb9077351d0b399
Carson Wardlow <86981998+draaaa@users.noreply.github.com>
2026-08-08 08:13:11 -0400

    Add files via upload

---
+++ hyprland.lua
@@ -0,0 +1,5 @@
+require("modules.binds")    -- keybinds
+require("modules.monitors") -- monitor rule stuff
+require("modules.autoexec") -- awww, waybar, etc
+require("modules.input")
+require("modules.environment")
